0e0143ed38f704a0c0bc0159530a79148c057f66
subhashini1411 <2021uee1694@mnit.ac.in>
2024-03-07 22:57:07 +0530

    testing for making pull request

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,58 @@ const option = {
     },
   },
 };
+
+/**
+ * const options = {
+    plugins: {
+      legend: {
+        display: true
+      }
+    },
+    layout: {
+      padding: {
+        left: 5,
+        right: 5,
+        top: 10,
+        bottom: 5
+      },
+      margin: {
+        left: 5,
+        right: 5,
+        top: 5,
+        bottom: 5
+      }
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+        border: { dash: [6, 6], display: true },
+        grid: {
+          display: true // Display grid lines for the y-axis
+        },
+        ticks: {
+          padding: 15
+        }
+      },
+      x: {
+        beginAtZero: true,
+        border: { display: true },
+        grid: {
+          display: false // Display grid lines for the y-axis
+        },
+        ticks: {
+          padding: 7
+        }
+      }
+    },
+    elements: {
+      bar: {
+        borderRadius: 40,
+        borderWidth: 0.7
+      }
+    }
+  };
+ */
 const data = {
   labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
   datasets: [{
