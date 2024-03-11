00793338cf3a5a0e6031b5e2e81bb3bc7391cb30
Flamer95711 <kumaraliv95711@gmail.com>
2024-03-11 21:32:36 +0530

    adding a admin page and making funtionality of signin and signup page

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,22 @@ import './App.css'
 import { BarChart } from './component/barChart'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import SignInPage from './authantication/signin'
+import SignUpPage from './authantication/signup'
+import NavBar from './component/Navbar'
+import Admin from './component/Admin'
 
 export default function App() {
   return (
     <div className="App">
       <Router>
         {/* <BarChart className="Bar" /> */}
-        <SignInPage />
+        {/* <SignInPage/> */}
         <Routes>
-          <Route exact path="/locality" element=""></Route> //exact path is not
-          fixed
+          <Route exact path="/" element={<NavBar />} />
+         <Route path='/admin' element ={<Admin />}/>
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/locality" element=""></Route> //exact path is not fixed
           <Route path="/locality/:id" element=""></Route>
         </Routes>
       </Router>
